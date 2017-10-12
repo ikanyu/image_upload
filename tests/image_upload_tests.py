46b04e5f9d21f9bc577bd83cb89720c943bedfa1
WenYu <wongwy93@gmail.com>
2017-10-12 14:51:27 +0800

    Add imports to test

--- tests/image_upload_tests.py
+++ tests/image_upload_tests.py
@@ -1,7 +1,10 @@
 import os
 import unittest
 
+from IPython import embed
+from io import BytesIO
+from flask import Flask, Request, request
+
 class ImageUploadTestCase(unittest.TestCase):
     def test_successful_upload(self):
-        """docstring for test_successful_upload"""
-        
+        assert true == true
