455ec5b2c1ffad99dd5868d2baca8c35d1999b17
WenYu <wongwy93@gmail.com>
2017-10-12 14:31:34 +0800

    basic functionality done

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, jsonify
+from IPython import embed
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    UPLOAD_FOLDER=os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+))
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload',methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify(
+            {
+                'message': 'File is not uploaded'
+            }
+        )
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify(
+            {
+                'message': 'Upload successful.',
+                'location': app.config['UPLOAD_FOLDER'] + filename
+            }
+        )
+    else:
+        return jsonify(
+            {
+                'message': 'Uploaded file is not a valid image.'
+            }
+        )
+
 if __name__ == "__main__":
     app.run(port=5000)
+
---
+++ tests/image_upload_tests.py
@@ -0,0 +1,7 @@
+import os
+import unittest
+
+class ImageUploadTestCase(unittest.TestCase):
+    def test_successful_upload(self):
+        """docstring for test_successful_upload"""
+        
